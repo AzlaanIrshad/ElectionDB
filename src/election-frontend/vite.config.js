0b5d7e366458259eb673be96c4f3cda20af2b3dd
Azlaan <azlaan.irshad@hva.nl>
2024-10-10 22:15:38 +0200

    Added hot reload to docker container, ensuring frontend file updates when code is edited

--- src/election-frontend/vite.config.js
+++ src/election-frontend/vite.config.js
@@ -1,16 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)) // Alias for src folder
     }
+  },
+  server: {
+    host: true, // Enables access over network and from within Docker container
+    watch: {
+      usePolling: true,
+    },
+    port: 5173, // vite port
+    strictPort: true
   }
-})
+});
