f872ec66846be2bc5fb7f1cf94b0ac163118d6aa
Erdem Berk Sahan <erdem.sahan_@hotmail.com>
2024-10-13 21:45:37 +0200

    added the page to the router

--- src/election-frontend/src/router.js
+++ src/election-frontend/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from "./views/HomePage.vue";
 import LoginPage from "./views/LoginPage.vue";
+import AdminPage from "./views/AdminPage.vue"; // Import the AdminPage component
 
 const routes = [
     {
@@ -12,8 +13,13 @@ const routes = [
         path: '/login',
         name: 'login',
         component: LoginPage,
+    },
+    {
+        path: '/admin',
+        name: 'admin',
+        component: AdminPage, // Define route for the admin page
+        meta: { requiresAuth: true } // Add meta to define it requires authentication (if applicable)
     }
-
 ];
 
 const router = createRouter({
