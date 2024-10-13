0c221e17706f37cf0d56699cbbf7ebe190e80cf1
Erdem Berk Sahan <erdem.sahan_@hotmail.com>
2024-10-13 22:03:47 +0200

    find user by role, optional

--- src/election-backend/src/main/java/repository/UserRepository.java
+++ src/election-backend/src/main/java/repository/UserRepository.java
@@ -2,7 +2,9 @@ package repository;
 
 import entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+    List<User> findByRole(String role);
 }
