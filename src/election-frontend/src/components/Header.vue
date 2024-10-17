1b3dbf919f735a1a7d9f62af324462b733b6b2d3
Ersin Karaduman <ersin.karaduman@hva.nl>
2024-10-17 09:31:29 +0200

    Improve homepage
    - Added left and right arrows to the carousel.
    - Improved responsiveness of the layout for better viewing on devices.
    - Added images.
    - Made it look nice.
    - Included <a> tags for navigation.

--- src/election-frontend/src/components/Header.vue
+++ src/election-frontend/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="bg-gray-900 py-6 text-white shadow-lg">
       <div class="container mx-auto flex justify-between items-center px-6">
-        <div class="logo text-3xl font-bold">Electiondb</div>
+        <a href="/"><div class="logo text-3xl font-bold">Electiondb</div></a>
       </div>
     </header>
   </template>
