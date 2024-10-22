bed1c4e74138f9511d51910258c1f62454526bfa
Ersin Karaduman <ersin.karaduman@hva.nl>
2024-10-22 17:21:28 +0200

    added party

---
+++ src/election-backend/src/main/java/com/example/parser/election/Party.java
@@ -0,0 +1,34 @@
+package com.example.parser.election;
+
+import java.util.List;
+
+public class Party {
+    private String name;
+    private int totalVotes;
+    private List<Candidate> candidates;
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalVotes() {
+        return totalVotes;
+    }
+
+    public void setTotalVotes(int totalVotes) {
+        this.totalVotes = totalVotes;
+    }
+
+    public List<Candidate> getCandidates() {
+        return candidates;
+    }
+
+    public void setCandidates(List<Candidate> candidates) {
+        this.candidates = candidates;
+    }
+}
