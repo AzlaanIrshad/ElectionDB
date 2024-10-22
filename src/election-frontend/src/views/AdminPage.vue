09afee0bdd915b0fd9df76798fa94bf2e2b89fdc
Erdem Berk Sahan <erdem.sahan_@hotmail.com>
2024-10-22 17:52:03 +0200

    removed button

--- src/election-frontend/src/views/AdminPage.vue
+++ src/election-frontend/src/views/AdminPage.vue
@@ -37,12 +37,7 @@
           >
             {{ user.active ? 'Deactivate' : 'Activate' }}
           </button>
-          <button
-              @click="deleteUser(user.id)"
-              class="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-4 rounded w-32"
-          >
-            Delete
-          </button>
+
         </td>
       </tr>
       </tbody>
