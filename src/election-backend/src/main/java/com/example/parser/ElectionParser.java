62bfd3590d97d4eba061da3e7570ea94af05655e
Ersin Karaduman <ersin.karaduman@hva.nl>
2024-11-06 23:37:00 +0100

    close parser when done

--- src/election-backend/src/main/java/com/example/parser/ElectionParser.java
+++ src/election-backend/src/main/java/com/example/parser/ElectionParser.java
@@ -5,12 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ElectionParser implements CommandLineRunner {
 
     @Autowired
     private ElectionService electionService;
+
+    @Autowired
+    private ApplicationContext context;
 
     public static void main(String[] args) {
         SpringApplication.run(ElectionParser.class, args);
@@ -21,5 +25,7 @@ public class ElectionParser implements CommandLineRunner {
         System.out.println("Starting XML to JSON parsing process...");
         electionService.parseXmlToJson();
         System.out.println("Parsing process completed.");
+
+        SpringApplication.exit(context, () -> 0);
     }
 }
