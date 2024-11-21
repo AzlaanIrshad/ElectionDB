7ecfb566f79966cc8330738f9489bef614955d0c
Ersin Karaduman <ersin.karaduman@hva.nl>
2024-11-21 20:50:36 +0100

    changed ports run script

--- start-servers.sh
+++ start-servers.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Start the backend server on port 8651
+# Start the backend server on port 8000
 cd backend
-java -jar election-backend-1.0-SNAPSHOT.jar --server.port=8651 &
+java -jar election-backend-1.0-SNAPSHOT.jar --server.port=8000 &
 BACKEND_PID=$!
 
-# Start the frontend server on port 8652
+# Start the frontend server on port 8001
 cd ../frontend
-npx http-server -p 8652 &
+npx http-server -p 8001 &
 FRONTEND_PID=$!
 
 # Wait for both processes to terminate
