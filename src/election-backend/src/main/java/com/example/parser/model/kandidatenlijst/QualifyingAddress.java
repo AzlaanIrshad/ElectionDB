bff96b310f441cdb8b8d8c2ff25cb3be71a99070
Ersin Karaduman <ersin.karaduman@hva.nl>
2024-11-24 17:08:52 +0100

    fixed canidate country

--- src/election-backend/src/main/java/com/example/parser/model/kandidatenlijst/QualifyingAddress.java
+++ src/election-backend/src/main/java/com/example/parser/model/kandidatenlijst/QualifyingAddress.java
@@ -1,5 +1,7 @@
 package com.example.parser.model.kandidatenlijst;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
@@ -9,8 +11,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @XmlAccessorType(XmlAccessType.FIELD)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class QualifyingAddress {
 
     @XmlElement(name = "Locality", namespace = "urn:oasis:names:tc:ciq:xsdschema:xAL:2.0")
     private Locality locality;
+
+    @XmlElement(name = "Country", namespace = "urn:oasis:names:tc:ciq:xsdschema:xAL:2.0", nillable = true)
+    private Country country;
+
+    @JsonProperty("countryPresent")
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    public Boolean isCountryPresent() {
+        return country != null && country.getCountryNameCode() != null;
+    }
 }
