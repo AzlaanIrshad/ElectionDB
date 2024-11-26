34541536ed64b12cb1f88dcf87b28dc6985457c7
Erdem Berk Sahan <erdem.sahan_@hotmail.com>
2024-11-26 15:18:27 +0100

    wip, delete user doesnt work yet but its a start

--- src/election-backend/src/main/java/com/example/service/UserService.java
+++ src/election-backend/src/main/java/com/example/service/UserService.java
@@ -4,7 +4,6 @@ import com.example.entity.User;
 import com.example.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.util.JwtUtil;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,32 +14,10 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private JwtUtil jwtUtil;
-
-    public String authenticate(String email, String password) {
-        Optional<User> userOptional = userRepository.findByEmail(email);
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            if (user.getPassword().equals(password)) {
-                System.out.println("User Role: " + user.getRole().name());
-                return jwtUtil.generateToken(user.getEmail(), user.getRole().name());
-            }
-        }
-        return null;
-    }
-
-    public void saveUser(User user) {
-        userRepository.save(user);
-    }
-
     public List<User> getUsers() {
         return userRepository.findAll();
     }
 
-    public boolean existsByEmail(String email) {
-        return userRepository.existsByEmail(email);
-    }
     public User toggleActiveStatus(Long id) {
         Optional<User> userOptional = userRepository.findById(id);
         if (userOptional.isPresent()) {
@@ -51,11 +28,13 @@ public class UserService {
         return null;
     }
 
-    public boolean deleteUser(Long id) {
-        if (userRepository.existsById(id)) {
-            userRepository.deleteById(id);
-            return true;
+    public boolean deleteUser(Long userId) {
+        User user = userRepository.findById(userId).orElse(null);
+        if (user == null) {
+            return false;
         }
-        return false;
+
+        userRepository.delete(user);
+        return true;
     }
 }
