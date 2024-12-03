44537f3cc37c97fce5063036a6c8277d91161d80
Ersin Karaduman <ersin.karaduman@hva.nl>
2024-12-03 07:35:18 +0100

    wip homepage

--- src/election-frontend/src/pages/HomePage.vue
+++ src/election-frontend/src/pages/HomePage.vue
@@ -1,68 +1,192 @@
 <template>
   <div class="homepage bg-gray-100 dark:bg-gray-900 font-sans text-gray-900 dark:text-gray-100">
     <!-- Hero Sectie -->
-    <section class="relative max-h-[80vh] h-screen flex items-center justify-center">
+    <section class="relative max-h-[97vh] h-screen flex items-center justify-center">
       <div
-          class="relative w-full h-full bg-cover bg-center shadow-lg flex flex-col md:flex-row"
+          class="relative w-full h-full bg-cover bg-center shadow-lg flex flex-col lg:flex-row"
           style="background-image: url('https://r.mobirisesite.com/963736/assets/images/photo-1631039302236-14327f30683a.jpeg');"
       >
-        <!-- Overlay voor leesbaarheid -->
         <div class="absolute inset-0 bg-black bg-opacity-30"></div>
 
-        <!-- Linker Content (geplaatst naar onderen links) -->
-        <div class="absolute bottom-8 left-8 z-10 bg-white bg-opacity-90 p-8 rounded-lg shadow-md max-w-lg">
-          <p class="text-lg font-medium text-gray-600 mb-2">Your Election Hub</p>
-          <h1 class="text-3xl md:text-5xl font-extrabold text-gray-900 mb-4 leading-tight">
-            Election Results
+        <div class="bg-blue-700 dark:bg-blue-800 absolute bottom-8 left-8 z-10 bg-opacity-90 p-6 sm:p-8 rounded-lg shadow-md max-w-[40rem] w-full">
+          <p class="text-sm sm:text-base font-medium text-white mb-2">Jouw Election Hub</p>
+          <h1 class="text-3xl sm:text-4xl md:text-5xl font-extrabold text-white mb-4 leading-tight">
+            Welkom bij ElectionDB
           </h1>
-          <p class="text-sm md:text-base text-gray-700 mb-6">
-            Stay informed on election outcomes. Explore results, maps, and real-time updates from the world of politics.
+          <p class="text-sm sm:text-base text-white mb-6">
+            Blijf op de hoogte met VerkiezingsDB 2024, jouw ultieme platform voor real-time verkiezingsupdates en deskundige analyses.
           </p>
           <a
-              href="/"
-              class="bg-blue-600 text-white px-4 py-2 rounded-full text-sm md:text-base font-medium hover:bg-blue-700 transition duration-300"
+              href="/dashboard"
+              class="bg-blue-6\700 text-white px-4 py-2 rounded-full text-sm sm:text-base font-medium hover:bg-blue-700 transition duration-300 border border-white"
           >
-            Explore Now
+            Dashboard
           </a>
         </div>
 
-        <!-- Rechter Content: Kaarten (blijft rechts) -->
-        <div class="absolute top-0 right-0 p-8 flex flex-col space-y-6 w-full md:w-1/2">
-          <!-- Kaart 1 -->
-          <div class="bg-white bg-opacity-90 p-6 rounded-lg shadow-md border border-gray-200">
-            <span class="text-2xl md:text-3xl font-bold text-gray-500">01</span>
-            <h2 class="text-sm md:text-lg font-bold text-gray-800 mb-1">Interactive Maps</h2>
-            <p class="text-xs md:text-sm text-gray-600">
-              Navigate through our dynamic maps that visualize election results by region. It’s like Google Maps, but for democracy!
+        <div class="absolute top-0 right-0 p-4 sm:p-6 lg:p-8 flex flex-col space-y-6 w-full sm:w-3/4 lg:w-1/2">
+          <router-link
+              to="/election-statistiek/per-stemlocatie"
+              class="bg-opacity-90 p-4 sm:p-6 rounded-lg shadow-md bg-blue-700 dark:bg-blue-800 text-white"
+          >
+            <span class="text-2xl sm:text-3xl font-bold">01</span>
+            <h2 class="text-sm sm:text-base md:text-lg font-bold dark:text-white mb-1">Interactieve Kaarten</h2>
+            <p class="text-xs sm:text-sm dark:text-white">
+              Navigeer door onze dynamische kaart die verkiezingsresultaten per stad weergeeft en de leidende partij van de verkiezingen zien .
             </p>
-          </div>
+          </router-link>
 
-          <!-- Kaart 2 -->
-          <div class="bg-white bg-opacity-90 p-6 rounded-lg shadow-md border border-gray-200">
-            <span class="text-2xl md:text-3xl font-bold text-gray-500">02</span>
-            <h2 class="text-sm md:text-lg font-bold text-gray-800 mb-1">Vote Analysis</h2>
-            <p class="text-xs md:text-sm text-gray-600">
-              Understand voter behavior with in-depth analytics.
+          <router-link
+              to="/election-statistiek/per-verkiezing"
+              class="bg-opacity-90 p-4 sm:p-6 rounded-lg shadow-md bg-blue-700 dark:bg-blue-800"
+          >
+            <span class="text-2xl sm:text-3xl font-bold text-white">02</span>
+            <h2 class="text-sm sm:text-base md:text-lg font-bold text-white mb-1">Totaal Stemmen Nederland</h2>
+            <p class="text-xs sm:text-sm text-white">
+              Zie hier de totale stemmen per verkiezingsjaar in Nederland van meerdere jaren.
             </p>
-          </div>
+          </router-link>
 
-          <!-- Kaart 3 -->
-          <div class="bg-white bg-opacity-90 p-6 rounded-lg shadow-md border border-gray-200">
-            <span class="text-2xl md:text-3xl font-bold text-gray-500">03</span>
-            <h2 class="text-sm md:text-lg font-bold text-gray-800 mb-1">Election News</h2>
-            <p class="text-xs md:text-sm text-gray-600">
-              Stay updated with real-time election headlines.
+          <router-link
+              to="/election-statistiek/zetels-per-partij"
+              class="bg-blue-700 dark:bg-blue-800 bg-opacity-90 p-4 sm:p-6 rounded-lg shadow-md"
+          >
+            <span class="text-2xl sm:text-3xl font-bold text-white">03</span>
+            <h2 class="text-sm sm:text-base md:text-lg font-bold text-white mb-1">Aantal Zetels Per Partij</h2>
+            <p class="text-xs sm:text-sm text-white">
+              Hier vind je de zetels per partij over de jaren heen en hoe deze zich hebben ontwikkeld.
             </p>
-          </div>
-          <div class="bg-white bg-opacity-90 p-6 rounded-lg shadow-md border border-gray-200">
-            <span class="text-2xl md:text-3xl font-bold text-gray-500">03</span>
-            <h2 class="text-sm md:text-lg font-bold text-gray-800 mb-1">Election News</h2>
-            <p class="text-xs md:text-sm text-gray-600">
-              Stay updated with real-time election headlines.
+          </router-link>
+          <router-link
+              to="/election-statistiek/zetels-per-jaar"
+              class="bg-blue-700 dark:bg-blue-800 bg-opacity-90 p-4 sm:p-6 rounded-lg shadow-md"
+          >
+            <span class="text-2xl sm:text-3xl font-bold text-white">04</span>
+            <h2 class="text-sm sm:text-base md:text-lg font-bold text-white mb-1">Aantal Zetels Per Jaar</h2>
+            <p class="text-xs sm:text-sm text-white">
+              Hier vind hoeveel zetels er per jaar zijn behaald en hoe deze zich hebben ontwikkeld.
             </p>
+          </router-link>
+        </div>
+      </div>
+    </section>
+    <section>
+      <!-- Categorieën Sectie -->
+      <section class="categories py-10 mb-10 bg-white dark:bg-gray-800 rounded-lg shadow-lg mt-10 mx-2 lg:mx-5">
+        <h2 class="text-3xl lg:text-4xl text-center mb-8 font-extrabold text-gray-800 dark:text-gray-100">Verkiezingscategorieën</h2>
+        <div class="blocks flex flex-col md:flex-row justify-around gap-4 lg:gap-6 mx-4 sm:mx-6 lg:mx-10">
+          <div class="block w-full md:w-1/4 h-36 sm:h-44 flex justify-center items-center bg-blue-600 dark:bg-blue-700 text-center rounded-lg shadow-md cursor-pointer hover:bg-blue-800 dark:hover:bg-blue-600 transition-all text-white transform hover:scale-105">
+            <a href="/"><span class="font-bold text-xl lg:text-2xl">Nationaal Overzicht</span></a>
           </div>
+          <div class="block w-full md:w-1/4 h-36 sm:h-44 flex justify-center items-center bg-blue-600 dark:bg-blue-700 text-center rounded-lg shadow-md cursor-pointer hover:bg-blue-800 dark:hover:bg-blue-600 transition-all text-white transform hover:scale-105">
+            <a href="/dashboard"><span class="font-bold text-xl lg:text-2xl">Resultaten per Staat</span></a>
+          </div>
+          <div class="block w-full md:w-1/4 h-36 sm:h-44 flex justify-center items-center bg-blue-600 dark:bg-blue-700 text-center rounded-lg shadow-md cursor-pointer hover:bg-blue-800 dark:hover:bg-blue-600 transition-all text-white transform hover:scale-105">
+            <a href="/"><span class="font-bold text-xl lg:text-2xl">Kandidatenprofielen</span></a>
+          </div>
+        </div>
+      </section>
+    <!-- Carrousel Sectie -->
+    <div class="carousel my-8 relative overflow-hidden mx-4 sm:mx-8 md:scroll-mx-24 lg:mx-48 rounded-lg shadow-lg">
+      <div
+          class="carousel-images flex transition-transform duration-500"
+          :style="{ transform: `translateX(-${activeIndex * 100}%)` }"
+      >
+        <img
+            v-for="(image, index) in featuredImages"
+            :key="index"
+            :src="image"
+            class="hero-image w-full h-48 sm:h-64 lg:h-80 object-fill rounded-lg shadow-md"
+            alt="Uitgelichte Afbeelding"
+        />
+      </div>
+
+      <!-- Linker Pijl -->
+      <button
+          class="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white dark:bg-gray-700 text-gray-800 dark:text-gray-200 p-2 rounded-full shadow-lg hover:bg-gray-200 dark:hover:bg-gray-600 transition"
+          @click="prevImage"
+      >
+        &#10094;
+      </button>
+
+      <!-- Rechter Pijl -->
+      <button
+          class="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white dark:bg-gray-700 text-gray-800 dark:text-gray-200 p-2 rounded-full shadow-lg hover:bg-gray-200 dark:hover:bg-gray-600 transition"
+          @click="nextImage"
+      >
+        &#10095;
+      </button>
+    </div>
+
+    <!-- Tegels Sectie -->
+    <div class="tiles flex justify-center mt-5 gap-2 sm:gap-4 pb-8">
+      <div
+          v-for="(image, index) in featuredImages"
+          :key="index"
+          class="tile w-1/4 sm:w-1/5 h-24 sm:h-32 lg:h-40 bg-gray-200 dark:bg-gray-700 text-center rounded-lg shadow-md transition-all cursor-pointer hover:bg-blue-500 dark:hover:bg-blue-600 transform hover:scale-105"
+          :class="{ 'border-blue-500 dark:border-blue-400 border-4': activeIndex === index }"
+          @click="setActiveIndex(index)"
+      >
+        <img :src="image" alt="Thumbnail" class="w-full h-full object-cover rounded-lg" />
+      </div>
+    </div>
+    </section>
+
+    <!-- Nieuws Sectie -->
+    <section class="news py-12 bg-white dark:bg-gray-800 rounded-lg shadow-lg mx-2 lg:mx-5 mb-10">
+      <h2 class="text-3xl lg:text-4xl text-center mb-10 font-extrabold text-gray-800 dark:text-gray-100">Recente Verkiezingsnieuws</h2>
+      <div class="news-content flex flex-col md:flex-row items-center gap-8 mx-4 sm:mx-6 lg:mx-10">
+        <img
+            src="https://media.gettyimages.com/id/1368872054/nl/vector/online-news-search-and-reading-news-updates-news-websites-information-on-newspapers-public.jpg?s=612x612&w=0&k=20&c=iZdnx4rKpTsJiSK8tFNyE1xztBXHXpHoBEXdDdT4ZFw="
+            alt="Nieuwsafbeelding"
+            class="news-image w-full md:w-2/6 lg:mr-28 h-40 sm:h-60 object-cover rounded-lg shadow-md"
+        />
+        <div class="news-text w-full md:w-3/5">
+          <h3 class="text-2xl sm:text-3xl font-extrabold mb-5 text-gray-800 dark:text-gray-100">Nederlandse Verkiezingen Zien Opkomst van Nieuwe Politieke Partijen</h3>
+          <p class="mb-4 text-base sm:text-lg text-gray-600 dark:text-gray-300 leading-relaxed">
+            Nederland beleeft een fascinerend verkiezingsjaar waarin nieuwkomers in de politiek hun stempel drukken op het democratische proces.
+          </p>
+          <button
+              class="bg-blue-600 text-white px-6 py-2 rounded-full text-sm sm:text-base font-medium hover:bg-blue-700 transition duration-300"
+          >
+            Lees meer
+          </button>
         </div>
       </div>
     </section>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: 0,
+      featuredImages: [
+        "https://media.gettyimages.com/id/1327961273/nl/foto/the-hague-netherlands-general-interior-view-of-the-tweede-kamer-building-is-seen-during-the.jpg?s=612x612&w=0&k=20&c=FZv3KDWl4flKiXpm49-0QwZvXB9Sp2Cry3artg0Uw9o=",
+        "https://media.gettyimages.com/id/1793796325/nl/foto/the-hague-netherlands-a-woman-walks-past-campaign-posters-on-november-20-2023-in-the-hague.jpg?s=612x612&w=0&k=20&c=tyylwgIawxWevTh-6WcVutNA-yhlPcz9GLWkT6Lwzc8=",
+        "https://media.gettyimages.com/id/1165687569/nl/foto/voting-box-and-election-image-election.jpg?s=612x612&w=0&k=20&c=Xu3sE7U-wgcL-xWrS41yGXhMl6NLqw7xqsGNHIPzRbI=",
+      ],
+
+    };
+  },
+  methods: {
+    nextImage() {
+      this.activeIndex = (this.activeIndex + 1) % this.featuredImages.length;
+    },
+    prevImage() {
+      this.activeIndex = (this.activeIndex - 1 + this.featuredImages.length) % this.featuredImages.length;
+    },
+    setActiveIndex(index) {
+      this.activeIndex = index;
+    },
+  },
+  mounted() {
+    // Automatische rotatie van de carrousel
+    this.carouselInterval = setInterval(this.nextImage, 5000); // Elke 5 seconden
+  },
+  beforeDestroy() {
+    // Stop de intervaltimer wanneer de component wordt vernietigd
+    clearInterval(this.carouselInterval);
+  },
+};
+</script>
