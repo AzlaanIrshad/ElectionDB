8faa5c3c8f2d9a2e8c0ab8ed60258d9e348750a8
Ersin Karaduman <ersin.karaduman@hva.nl>
2024-12-10 22:06:13 +0100

    coordinates apart gedaan

--- src/election-frontend/src/components/ElectionMap.vue
+++ src/election-frontend/src/components/ElectionMap.vue
@@ -58,6 +58,8 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import config from "../config";
+import cityCoordinates from "../util/cityCoordinates";
+
 
 export default {
   name: "ElectionMap",
@@ -176,29 +178,10 @@ export default {
       });
     },
     getCoordinatesForCity(cityName) {
-      const coordinates = {
-        Amsterdam: [52.3676, 4.9041],
-        Arnhem: [51.9851, 5.8987],
-        Assen: [53.0000, 6.5667],
-        Bonaire: [12.1784, -68.2385],
-        "Den Helder": [52.9599, 4.7590],
-        Dordrecht: [51.8133, 4.6901],
-        Groningen: [53.2194, 6.5665],
-        Haarlem: [52.3874, 4.6462],
-        Leeuwarden: [53.2012, 5.7999],
-        Leiden: [52.1601, 4.4970],
-        Lelystad: [52.5185, 5.4714],
-        Maastricht: [50.8514, 5.6910],
-        Middelburg: [51.4988, 3.6106],
-        Nijmegen: [51.8422, 5.8528],
-        Rotterdam: [51.9225, 4.47917],
-        "'s-Gravenhage": [52.0705, 4.3007],
-        "'s-Hertogenbosch": [51.6978, 5.3037],
-        Tilburg: [51.5606, 5.0919],
-        Utrecht: [52.0907, 5.12142],
-        Zwolle: [52.5168, 6.0830],
-      };
-      return coordinates[cityName] || [52.3676, 4.9041];
+      if (!cityCoordinates[cityName]) {
+        console.warn(`Geen coördinaten beschikbaar voor: ${cityName}`);
+      }
+      return cityCoordinates[cityName] || null;
     },
     onYearChange() {
       this.fetchElectionResults();
