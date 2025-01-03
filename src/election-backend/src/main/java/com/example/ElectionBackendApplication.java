11d9bc75a7fbcb79ef2180411ca4ec7b85752ac8
Azlaan <azlaan.irshad@hva.nl>
2025-01-03 21:22:40 +0100

    Implemented security improvements and added documentation for ethical changes

--- src/election-backend/src/main/java/com/example/ElectionBackendApplication.java
+++ src/election-backend/src/main/java/com/example/ElectionBackendApplication.java
@@ -1,11 +1,22 @@
 package com.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ElectionBackendApplication {
     public static void main(String[] args) {
+        // Load environment variables from the .env.local file
+        Dotenv dotenv = Dotenv.configure().filename(".env.local").load();
+
+
+        // Set system properties to match Spring properties
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+        // Run Spring Boot application
         SpringApplication.run(ElectionBackendApplication.class, args);
     }
 }
