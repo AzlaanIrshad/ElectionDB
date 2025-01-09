27e743911320d968786758153ef9e4aef3c29026
Azlaan <azlaan.irshad@hva.nl>
2025-01-09 13:39:01 +0100

    added mapper to ThreadCommentRequest dto

--- src/election-backend/src/main/java/com/example/dto/ThreadCommentRequest.java
+++ src/election-backend/src/main/java/com/example/dto/ThreadCommentRequest.java
@@ -10,5 +10,13 @@ public class ThreadCommentRequest {
     private String body;
     private String date;
     private String email;
+
+    public ThreadComment toEntity() {
+        Thread thread = new Thread();
+        thread.setBody(body);
+        thread.setDate(date);
+        thread.setEmail(email);
+        return ThreadComment;
+    }
 }
 
